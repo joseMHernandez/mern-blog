8527046a653a4249a8225934486d40b99d73f787
josemhernandez <manuhdez010@gmil.com>
2024-04-09 18:44:09 -0400

    'pages & routers created'

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 
+import { BrowserRouter, Routes, Route  } from "react-router-dom";
+import About from "./pages/About";
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import Projects from './pages/Projects';
+import DashBoard from './pages/DashBoard';
+import SingUp from './pages/SingUp';
 const App = () => {
   return (
-    <h1 className='text-3xl text-red-400'>Hello</h1>
-  )
+<BrowserRouter>
+
+<Routes>
+
+  <Route path='/' element={<Home/>}/>
+  <Route path='/about' element={<About/>}/>
+  <Route path='/sign-in' element={<SignIn/>}/>
+  <Route path='/sign-up' element={<SingUp/>}/>
+  <Route path='/dashboard' element={<DashBoard/>}/>
+  <Route path='/projects' element={<Projects/>}/>
+
+</Routes>
+
+</BrowserRouter>  
+
+)
 
 }
 
