98d46cde3643df65fa8d6a882c365e451358915c
josemhernandez <manuhdez010@gmil.com>
2024-04-09 21:00:36 -0400

    database connection

--- back/index.js
+++ back/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 
-
+const connectDB = require('./db/connect')
+require('dotenv').config('')
 
 
 const app = express()
@@ -12,7 +13,21 @@ res.send('hello')
 })
 
 
-const port = 3000
-app.listen(port, ()=>{
-    console.log(`server is listening on port ${port}..`)
-})
+
+
+
+const start = async () =>{
+
+try {
+    await connectDB(process.env.mongoURL)
+    app.listen(3000, ()=>{
+        console.log(`server is listening on port 3000..`)
+    })
+    
+} catch (error) {
+    console.log(error)
+}
+
+}
+
+start()
