0e7a66d3b3f81e16df063b5c2a6a391edc34e45f
josemhernandez <manuhdez010@gmil.com>
2024-04-09 20:26:14 -0400

    create and run the server

---
+++ back/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+
+
+
+const app = express()
+
+
+
+app.get('/',(req, res)=>{
+res.send('hello')
+})
+
+
+const port = 3000
+app.listen(port, ()=>{
+    console.log(`server is listening on port ${port}..`)
+})
